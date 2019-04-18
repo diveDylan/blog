6f49a37aabfa14e4564b22a69e7dcde8f6f051c7
曾祥达 <zengxiangda@zhongan.io>
2019-04-19 00:38:03 +0800

    Updates

---
+++ precache-manifest.5ae3e153f96069b2287856d28ae61795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4599b83324fce8072cee",
+    "url": "/static/js/main.4e7ad1a8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "4599b83324fce8072cee",
+    "url": "/static/css/main.04b02eeb.chunk.css"
+  },
+  {
+    "revision": "98990cef0630009d1ba113bffc076800",
+    "url": "/index.html"
+  }
+];
